e5619b658802b68f301c562c41728f4b08e2f52b
russkh <kharuba@gmail.com>
2024-04-30 22:22:11 -0600

    json fix

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,1 +1,47 @@
-import {Col, Form, Row} from "react-bootstrap";import storeItems from '../data/items.json'import {StoreItem} from "../components/StoreItem.tsx";import {useEffect} from "react";import {useShoppingCart} from "../context/ShoppingCartContext.tsx";export function Store() {    // const [searchText, setSearchText] = useState('')const {searchTerm, setSearchTerm, filteredItems, setFilteredItems } = useShoppingCart()    useEffect(()=> {        const lowerCaseSearchTerm = searchTerm.toLowerCase();const filtered = storeItems.filter( item => item.name.toLowerCase().includes(lowerCaseSearchTerm))   setSearchTerm(searchTerm);setFilteredItems(filteredItems)    }, [searchTerm])    return (        <>            <h1>I'm a store! Buy smth</h1>            <Form className="d-flex m-3">                <Form.Control                    placeholder="Search for items"                    type="text"                    value={searchTerm}                    onChange={(e) => setSearchTerm(e.target.value)}                />            </Form>            <Row xs={1} md={2} lg={3}>                {filteredItems.map(item =>                    <Col key={item.id}>                        <StoreItem {...item}/>                    </Col>)}            </Row>        </>    )}
+import Form from "react-bootstrap/Form";
+import { Col, Row } from "react-bootstrap";
+import storeItems from "../data/items.json";
+import { StoreItem } from "../components/StoreItem.tsx";
+import { useShoppingCart } from "../context/ShoppingCartContext.tsx";
+import { useEffect } from "react";
+
+export function Store() {
+  const { searchTerm, setSearchTerm, filteredItems, setFilteredItems } =
+    useShoppingCart();
+
+  useEffect(() => {
+    const lowerCaseSearchTerm = searchTerm.toLowerCase();
+    const filtered = storeItems.filter((item) =>
+      item.name.toLowerCase().includes(lowerCaseSearchTerm)
+    );
+
+    setSearchTerm(searchTerm);
+    setFilteredItems(filtered);
+  }, [searchTerm]);
+
+  return (
+    <>
+      <h1>Store Page is Here</h1>
+
+      <Form className="d-flex">
+        <Form.Control
+          placeholder="Search for items"
+          type="text"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+        />
+      </Form>
+
+      <Row xs={1} md={2} lg={3}>
+        {filteredItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem
+              // item={item}
+              {...item}
+            />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+}
